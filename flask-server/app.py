7abb6657d0fc0622836bf26e672521b4f4e90e27
Gaurav Bhalerao <gauravbhalerao107@gmail.com>
2022-06-19 18:59:39 +0530

    flask server setup

---
+++ flask-server/app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from models import *
+from functools import wraps
+from flask_cors import CORS
+from pymongo import MongoClient
+from utils import JSONEncoder
+
+app = Flask(__name__)
+app.json_encoder = JSONEncoder
+cors = CORS(app)
+
+class Config(object):
+  MONGODB_HOST = "mongodb+srv://<username>:<password>@cluster0.kbwcx.mongodb.net/<database_name>?retryWrites=true&w=majority"
+ 
+app.config.from_object(Config())
+db.init_app(app)
+
+# decorators
+def master_admin_auth(f):
+  @wraps(f)
+  def decorator(*args, **kwargs):
+    if 'master_admin_id' not in flask.session:
+      return flask.abort(401)
+    return f(*args, **kwargs)
+  return decorator
+
+def admin_auth(f):
+  @wraps(f)
+  def decorator(*args, **kwargs):
+    if 'admin_id' not in flask.session:
+      return flask.abort(401)
+    return f(*args, **kwargs)
+  return decorator
+
+# view blueprints
+from views.admin_view import *
+from views.master_admin_view import *
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=5000)
